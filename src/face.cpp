bd45403e52255923a2e5fed75d62e3f0072a5fd3
毕赛 <soundsilencebisai@gmail.com>
2014-02-16 23:52:23 +0800

    add class definition

---
+++ src/face.cpp
@@ -0,0 +1,18 @@
+/**
+ * @author 
+ * @version 2014/02/16
+ */
+
+#include "face.h"
+
+ 
+Face::Face(){
+    
+}
+
+void Face::getMeanShape(){
+    
+    for(int i = 0;i < trainingNum;i++){
+
+    }
+}
--- src/face.h
+++ src/face.h
@@ -3,10 +3,47 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include "cv.h"
-using namespace std;
-using namespace cv;
 
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+using namespace boost::numeric::ublas;
+using namespace std;
+
+
+class Face{
+    public:
+        //grayscale training images
+        vector<matrix<double>> trainingImages;
+        //grayscale testing images 
+        vector<matrix<double>> testingImages;
+        //mean shape of training images
+        vector<double> meanShape;
+        
+        
+        vector<vector<double>> trainingFeatures;
+        vector<vector<double>> testingFeatures;
+        
+        Face();
+
+        //get the meanShape
+        void getMeanShape();
+        
+        //extract feature
+        void extractFeature(matrix<double> iamge);
+        
+        //read from file
+        void readDataFromFile();
+        
+        //read parameters from parameters.ini
+        void readParameters();
+
+        
+        
+          
+};
 
 
 
