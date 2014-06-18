c8c4b78b7bb7b44a3669d4a99f13357a2a039b33
soundsilence <soundsilencebisai@gmail.com>
2014-06-19 00:21:48 +0800

    update readme

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ extremely fast both in training and testing.
 Please go to folder `FaceAlignment` to see the source files.
 
 ###Usage
+To compile the program(OpenCV required):
+```
+// Go to folder FaceAlignment
+cmake .
+make TrainDemo.out
+make TestDemo.out
+```
 To train a new model:
 ``` C++
 ShapeRegressor regressor;
@@ -20,6 +27,10 @@ regressor.load("./data/model_cofw_2.txt");
 regressor.Predict(test_images[index],bounding_box[index],initial_number);
 ```
 For details, please see `TrainDemo.cpp` and `TestDemo.cpp`.
+
+###Dataset
+A public dataset is provided [here](https://drive.google.com/file/d/0B0tUTCaZBkccUU5hVkNJTFB0VDQ/edit?usp=sharing). The dataset contains 1345 training images, 507 testing images, and each image has 29 landmarks. You can change the path
+in `TrainDemo.cpp` and `TestDemo.cpp` to train new models.
 
 ###Sample Results
 ![Sample Results](https://dl.dropboxusercontent.com/u/47747425/Photo/point1.png)
--- source_code/fern_cascade.cpp
+++ source_code/fern_cascade.cpp
@@ -128,7 +128,7 @@ void FernCascade::train(const vector<Mat_<uchar> >& images,
         translate_scale_rotate(normalized_shapes[i],mean_shape,translation,scale,rotation); 
        
         for(int j = 0;j < pixel_pair_num;j++){
-            double x = dixel_coordinates(j,0);
+            double x = pixel_coordinates(j,0);
             double y = pixel_coordinates(j,1);
             double project_x = rotation(0,0) * x + rotation(0,1) * y;
             double project_y = rotation(1,0) * x + rotation(1,1) * y;
@@ -167,6 +167,7 @@ void FernCascade::train(const vector<Mat_<uchar> >& images,
             double correlation_result = calculate_covariance(pixel_density[i],pixel_density[j]);
             correlation(i,j) = correlation_result;
             correlation(j,i) = correlation_result;
+        }
     }
 	// train ferns
     primary_fern_.resize(second_level_num);
@@ -206,11 +207,12 @@ void FernCascade::train(const vector<Mat_<uchar> >& images,
 
 void FernCascade::write(ofstream& fout){
     fout<<second_level_num_<<endl;
-
     for(int i = 0;i < second_level_num_;i++){
         primary_fern_[i].write(fout);
     }
 }
+
+
 void FernCascade::read(ifstream& fin){
     fin>>second_level_num_;
 
